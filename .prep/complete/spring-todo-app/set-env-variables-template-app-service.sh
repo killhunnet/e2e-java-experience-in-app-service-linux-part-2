3ffefc994268fef536fa75bf9e55a6e6ca17be2c
Moary Chen <v-moaryc@microsoft.com>
2021-05-28 12:58:20 +0800

    Update spring boot and azure spring boot version; update azure-webapp-maven-plugin version

--- .prep/complete/spring-todo-app/set-env-variables-template-app-service.sh
+++ .prep/complete/spring-todo-app/set-env-variables-template-app-service.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-export COSMOSDB_URI=<put-your-COSMOS-DB-documentEndpoint-URI-here>
-export COSMOSDB_KEY=<put-your-COSMOS-DB-primaryMasterKey-here>
-export COSMOSDB_DBNAME=<put-your-COSMOS-DB-name-here>
+export COSMOS_URI=<put-your-COSMOS-DB-documentEndpoint-URI-here>
+export COSMOS_KEY=<put-your-COSMOS-DB-primaryMasterKey-here>
+export COSMOS_DATABASE=<put-your-COSMOS-DATABASE-name-here>
 export RESOURCEGROUP_NAME=<put-your-resource-group-name-here>
 export WEBAPP_NAME=<put-your-Webapp-name-here>
 export REGION=<put-your-REGION-here>
+export SUBSCRIPTION_ID=<put-your-SUBSCRIPTION_ID-here>
